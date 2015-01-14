3b1e0545e6e5a9f369f2e1378fe22e04036d4ec2
joshych <yucheng1102@gmail.com>
2015-01-14 17:16:01 +0800

    fixed context switch and race condition

--- inc/mutex.h
+++
@@ -1,9 +0,0 @@
-#ifndef _mutex_h_
-#define _mutex_h_
-
-#define mutex_locked    1
-#define mutex_unlocked  0
-
-typedef unsigned int mutex_t;
-
-#endif /* _mutex_h_ */
---
+++ inc/spinlock.h
@@ -0,0 +1,9 @@
+#ifndef _spinlock_h_
+#define _spinlock_h_
+
+#define spinlock_locked    1
+#define spinlock_unlocked  0
+
+typedef unsigned int spinlock_t;
+
+#endif /* _spinlock_h_ */
