d2a0b798c69f73d00d1fb15ef8454a4403edb5e1
gogojesse <gogojesseco@gmail.com>
2015-04-22 10:59:14 +0800

    Implement usleep and refine xsleep()
    Signed-off-by: gogojesse <gogojesseco@gmail.com>

--- libc/unistd.c
+++ libc/unistd.c
@@ -2,12 +2,12 @@
 #include <sys/time.h>
 #include "inc/task.h"
 
-unsigned sleep(unsigned int __seconds)
+static int os_sleep(unsigned int __seconds, useconds_t __useconds)
 {
 	struct timeval tv1, tv2;
 	struct timezone tz1, tz2;
 
-	if (__seconds <= 0)
+	if (__useconds < 0)
 		return 0;
 
 	/* Get current time. */	
@@ -18,12 +18,31 @@ unsigned sleep(unsigned int __seconds)
 
 		/* check time expired. */
 		gettimeofday (&tv2, &tz2);
-	} while (((tv2.tv_sec - tv1.tv_sec) < __seconds) || (tv2.tv_usec < tv1.tv_usec));
+	} while (((tv2.tv_sec - tv1.tv_sec) < __seconds) || ((tv2.tv_usec - tv1.tv_usec) < __useconds));
 
 	return 0; /* requested time has elapsed */
 }
 
+unsigned sleep(unsigned int __seconds)
+{
+	struct timeval tv1, tv2;
+	struct timezone tz1, tz2;
+
+	if (__seconds == 0)
+		return 0;
+
+	return (unsigned)os_sleep(__seconds, 0);
+}
+
 int usleep(useconds_t __useconds)
 {
-	return 0;
+	unsigned int __seconds;
+
+	if (0 >= __useconds)
+		return 0;
+
+	__seconds = __useconds / 1000000;
+	__useconds = __useconds % 1000000;
+
+	return (int)os_sleep(__seconds, __useconds);
 }
