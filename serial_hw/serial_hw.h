f33b77ab456c7ac85d5ffddfa9fc60cc43abf200
gogojesse <gogojesseco@gmail.com>
2015-05-12 22:28:11 +0800

    Testing code for UART port for raspberry pi.
    Signed-off-by: gogojesse <gogojesseco@gmail.com>

--- serial_hw/serial_hw.h
+++ serial_hw/serial_hw.h
@@ -1,7 +1,12 @@
 #ifndef _serial_hw_h_
 #define _serial_hw_h_
 
+#if 0
 #define	UART01x_ADDR		0x101f1000
+#else
+#define	UART01x_ADDR		0x20201000
+#endif
+
 #define UART01x_DR              0x000
 #define UART01x_FR_BUSY         0x008
 #define	UART01x_FR_RXFE		0x010
