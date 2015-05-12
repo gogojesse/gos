a158eeafe5f1435c81b235b441496eb66688bb39
gogojesse <gogojesseco@gmail.com>
2015-05-12 22:26:31 +0800

    Add UART definition for raspberry pi.
    Signed-off-by: gogojesse <gogojesseco@gmail.com>

--- inc/platform-defs.h
+++ inc/platform-defs.h
@@ -1,6 +1,10 @@
 #ifndef __platform_defs__
 #define __platform_defs__
 
+#if 0
 #include "versatilepb-defs.h"
+#else
+#include "raspi-defs.h"
+#endif
 
 #endif /* __platform_defs__ */
---
+++ inc/raspi-defs.h
@@ -0,0 +1,7 @@
+#ifndef __raspi_defs__
+#define __raspi_defs__
+
+#define VIC_REGBASE	0x10140000
+#define RTC_REGBASE	0x101E8000
+#define UART0_REGBASE	0x07E20100
+#endif /* __versatilepb_defs__ */
