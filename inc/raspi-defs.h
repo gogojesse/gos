da4f54b4a3a535cd77d31e5d7bc416c914bd478c
gogojesse <gogojesseco@gmail.com>
2015-05-13 13:49:20 +0800

    Fix wrong base address definition in datasheet. Keep the definition for searching keyword easier.
    Signed-off-by: gogojesse <gogojesseco@gmail.com>

--- inc/raspi-defs.h
+++ inc/raspi-defs.h
@@ -3,5 +3,6 @@
 
 #define VIC_REGBASE	0x10140000
 #define RTC_REGBASE	0x101E8000
-#define UART0_REGBASE	0x07E20100
-#endif /* __versatilepb_defs__ */
+//#define UART0_REGBASE	0x07E20100   /* Wrong definition in datasheet. */
+#define UART0_REGBASE	0x20201000
+#endif /* __raspi_defs__ */
